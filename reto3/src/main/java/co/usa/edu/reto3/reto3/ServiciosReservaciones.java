884fceb33ffd842d4712103b28a60e315f75cbe9
Jennifer Sanchez <jennifer.sanchez.mt@correo.usa.edu.co>
2021-11-01 23:55:40 -0500

    first commit

---
+++ reto3/src/main/java/co/usa/edu/reto3/reto3/ServiciosReservaciones.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.usa.edu.reto3.reto3;
+/**
+ * Importaciones
+ */
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Jennifer
+ */
+@Service
+/**
+ * Servicio de reservaciones
+ */
+public class ServiciosReservaciones {
+    @Autowired
+    /**
+     * Metodo CRUD
+     */
+    private RepositorioReservaciones metodosCrud;
+    /**
+     * Lista
+     */
+    public List<Reservaciones> getAll(){
+        return metodosCrud.getAll();
+    }
+    /**
+     * get
+     */
+    public Optional<Reservaciones> getReservation(int reservationId) {
+        return metodosCrud.getReservation(reservationId);
+    }
+    /**
+     * save
+     */
+    public Reservaciones save(Reservaciones reservation){
+        if(reservation.getIdReservation()==null){
+            return metodosCrud.save(reservation);
+        }else{
+            Optional<Reservaciones> e= metodosCrud.getReservation(reservation.getIdReservation());
+            if(e.isEmpty()){
+                return metodosCrud.save(reservation);
+            }else{
+                return reservation;
+            }
+        }
+    }
+    /**
+     * update
+     */
+    public Reservaciones update(Reservaciones reservation){
+        if(reservation.getIdReservation()!=null){
+            Optional<Reservaciones> e= metodosCrud.getReservation(reservation.getIdReservation());
+            if(!e.isEmpty()){
+
+                if(reservation.getStartDate()!=null){
+                    e.get().setStartDate(reservation.getStartDate());
+                }
+                if(reservation.getDevolutionDate()!=null){
+                    e.get().setDevolutionDate(reservation.getDevolutionDate());
+                }
+                if(reservation.getStatus()!=null){
+                    e.get().setStatus(reservation.getStatus());
+                }
+                metodosCrud.save(e.get());
+                return e.get();
+            }else{
+                return reservation;
+            }
+        }else{
+            return reservation;
+        }
+    }
+    /**
+     * delete
+     */
+    public boolean deleteReservation(int reservationId) {
+        Boolean aBoolean = getReservation(reservationId).map(reservation -> {
+            metodosCrud.delete(reservation);
+            return true;
+        }).orElse(false);
+        return aBoolean;
+    }
+    
+}
